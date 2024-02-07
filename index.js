c656c4fa6b2300076f066f4e08a675f068c36bd5
swadebsharma6 <swadebriva2012@gmail.com>
2024-02-07 14:52:26 +0600

    added a server site and initial with express and mongodb

---
+++ index.js
@@ -0,0 +1,54 @@
+
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fikwith.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+  
+    await client.connect();
+
+    const productCollection = client.db('TotalBuyDB').collection('products');
+
+    app.get('/products', async(req, res)=>{
+        const cursor = productCollection.find();
+        const products = await cursor.toArray();
+        res.send(products)
+    })
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('total buy server is running')
+});
+
+app.listen(port, () => {
+  console.log(`total buy server is running on port ${port}`)
+});
